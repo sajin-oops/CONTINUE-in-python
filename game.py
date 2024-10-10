fc9aa0f0a83292b252415fb7881571113703ead0
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-10 19:19:05 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,19 @@
+
+for num in range(1, 8):  
+    if num == 5:
+        print("Found the number 5. continue the loop.")
+        continue  
+    print(num)
+
+'''
+O/P
+
+
+2
+3
+4
+Found the number 5. continue the loop.
+6
+7
+
+'''
